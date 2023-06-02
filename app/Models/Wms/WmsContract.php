8fcd8495f67b33366cff7eb403d7b8a6ab2623cd
chitushgyl <chitushgyl@163.com>
2023-06-02 15:25:36 +0800

    1

--- app/Models/Wms/WmsContract.php
+++ app/Models/Wms/WmsContract.php
@@ -66,7 +66,7 @@ class WmsContract extends Model{
     public function WmsBillType(){
         //参数：关联模型名称，外键，主键
         //如果主键是id可以省略
-        return $this->hasMany('App\Models\Wms\WmsBillType','bill_id','self_id');
+        return $this->hasMany('App\Models\Wms\WmsBillType','self_id','bill_id');
     }
 
 
