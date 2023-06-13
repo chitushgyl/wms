e4ce83db5cb88cfaa4c4aed79bdae0a723c57374
chitushgyl <chitushgyl@163.com>
2023-06-13 17:14:04 +0800

    1

--- app/Http/Admin/Wms/ChangeController.php
+++ app/Http/Admin/Wms/ChangeController.php
@@ -246,7 +246,7 @@ class ChangeController extends CommonController{
                     $list['good_weight']       =  $value['good_weight'];//件重
 //                $list['good_num']          =  $value['good_num'];//件数
                     $list['weight']            =  $value['weight'];//吨重
-                    $list['num']               =  $value['number'];//数量
+                    $list['num']               =  $value['num'];//数量
                     $list['plate_num']         =  $value['plate_num'];//板数
                     $list['plate_id']          =  $value['plate_id'];//板位
                     $list['inventory_num']     =  $value['inventory_num'];//库存件数
@@ -259,7 +259,7 @@ class ChangeController extends CommonController{
                     }
 
                     $wmsLibrarySige = WmsLibrarySige::where('self_id',$value['sige_id'])->first();
-                    $library_sige['num']       = $wmsLibrarySige->num - $value['number'];
+                    $library_sige['num']       = $wmsLibrarySige->num - $value['num'];
                     $library_sige['warehouse_id']       = $value['warehouse_id'];
                     $library_sige['warehouse_name']       = $value['warehouse_name'];
 
